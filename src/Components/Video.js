bff5149cacae74e66420b5d8c0079d47a488e945
Margarita Morozova <ritamorozova@yahoo.com>
2020-12-02 20:57:27 -0800

    somw minor fixes

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
+import  '../styles/Video.css'
+
 
 class Video extends React.Component {
 
@@ -11,12 +13,18 @@ class Video extends React.Component {
     }
 
     return(
-      <div>
-        <ReactPlayer url={video.url} />
+      <div className='standard'>
+        <ReactPlayer url={video.url} 
+          light={true} 
+          width='300px'
+          height='180px'
+          origin='http://localhost:3000'
+          />
         <h2>{video.name}</h2>
         <button onClick={() => this.props.addToList(this.props.video)}>Add To My List</button>
         <button onClick={handleClick}>Go to this lesson</button>
-      </div>
+
+        </div>
     )
   }
 }
