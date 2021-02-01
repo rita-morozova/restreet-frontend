4913fcc5437b05fb72309dbd3797247f679d89d6
Margarita Morozova <ritamorozova@yahoo.com>
2021-01-31 19:20:05 -0800

    add regex validations for email

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -9,9 +9,9 @@ import {
 } from "semantic-ui-react";
 import "../styles/Form.css";
 
-// const validEmailRegex = RegExp(
-//   /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-// );
+const validEmailRegex = RegExp(
+  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+);
 const validateForm = (errors) => {
   let valid = true;
   Object.values(errors).forEach((val) => val.length > 0 && (valid = false));
@@ -50,9 +50,9 @@ class Signup extends React.Component {
         errors.username =
           value.length < 4 ? "Username must be at least 4 characters long" : "";
         break;
-      // case "email":
-      //   errors.email = validEmailRegex.test(value) ? "" : "Email is not valid";
-      //   break;
+      case "email":
+        errors.email = validEmailRegex.test(value) ? "" : "Email is not valid";
+        break;
       case "password":
         errors.password =
           value.length < 6 ? "Password must be at least 6 characters long" : "";
